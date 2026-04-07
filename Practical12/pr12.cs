64945658aaf04da3fe8b6b617340032c46ddf22a
IshanChipate <ishanchipate@gmail.com>
2026-04-07 22:45:26 +0530

    Added practical 11-15

---
+++ Practical12/pr12.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        int n, a = 0, b = 1, c;
+
+        Console.Write("Enter number of terms: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        Console.ReadKey();
+    }
+}

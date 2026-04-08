27e5eee95b19803a36961ebdbdc1feb11be5db3a
IshanChipate <ishanchipate@gmail.com>
2026-04-08 23:32:53 +0530

    Added Practical 71 to 80

---
+++ Practical80/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class StaticMethodDemo
+{
+    static void DisplayMessage()
+    {
+        Console.WriteLine("This is a static method");
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        DisplayMessage();
+
+        int result = Add(10, 20);
+        Console.WriteLine("Sum = " + result);
+
+        Console.ReadKey();
+    }
+}
